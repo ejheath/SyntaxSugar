e8da40476ad2e7183218ed5b87189c55d9cceccc
Elijah Heath <ejheath8888@gmail.com>
2022-02-15 12:42:24 -0500

    syntax sugar exercise

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+        }
+    }
+}
+
+
+/* int answer = 4;
+string response;
+if (answer = 9)
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + “greater than or equal to nine”;
+}
+*/
